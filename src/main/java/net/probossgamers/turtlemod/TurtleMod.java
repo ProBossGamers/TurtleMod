d2790aea50b0c21aea12372edf3e3dd4a8fa4311
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-09 22:09:32 +1000

    Easier entity adding

--- src/main/java/net/probossgamers/turtlemod/TurtleMod.java
+++ src/main/java/net/probossgamers/turtlemod/TurtleMod.java
@@ -7,8 +7,10 @@ import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.util.EnumHelper;
 import net.probossgamers.turtlemod.entity.monster.EntityMineTurtle;
 import net.probossgamers.turtlemod.entity.monster.EntityZombieTurtle;
@@ -60,9 +62,10 @@ public class TurtleMod {
                 return turtleShell;
             }
         };
-        registerEntity(EntityTurtle.class, "turtle", 0x1e8100, 0x7d3900);
-        registerEntity(EntityZombieTurtle.class, "zombieTurtle", 0x008344, 0x823F02);
-        registerEntity(EntityMineTurtle.class, "mineTurtle", 0x1e8100, 0xef0000);
+        registerEntity(EntityTurtle.class, "turtle", 0x1e8100, 0x7d3900, 10, 2, 4, EnumCreatureType.creature);
+        registerEntity(EntityZombieTurtle.class, "zombieTurtle", 0x008344, 0x823F02, 10, 2, 4, EnumCreatureType.monster);
+        registerEntity(EntityMineTurtle.class, "mineTurtle", 0x1e8100, 0xef0000, 10, 2, 4, EnumCreatureType.monster);
+
         turtleLeather = new Item().setUnlocalizedName("turtleLeather").setTextureName("turtlemod:turtleLeather").setCreativeTab(turtleTab);
         turtleShell = new ItemTurtleShell(turtleMaterial, turtleShellid, 1).setTextureName("turtlemod:turtleShell").setUnlocalizedName("turtleShell");
         turtleHelmet = new ItemTurtleArmor(turtleMaterial, turtleHelmetid, 0).setTextureName("turtlemod:turtleHelmet").setUnlocalizedName("turtleHelmet");
@@ -87,11 +90,20 @@ public class TurtleMod {
 
     }
 
-    public static void registerEntity(Class<? extends net.minecraft.entity.Entity> entityClass, String name, int par3, int par4)
+    public static void addSpawn(Class par1, int par2, int par3, int par4, EnumCreatureType par5) {
+
+    }
+
+    public static void registerEntity(Class entityClass, String name, int par1, int par2, int par3, int par4, int par5, EnumCreatureType CreatureType)
     {
         int entityID = EntityRegistry.findGlobalUniqueEntityId();
         EntityRegistry.registerGlobalEntityID(entityClass, name, entityID);
         EntityRegistry.registerModEntity(entityClass, name, entityID, instance, 64, 1, true);
-        EntityList.entityEggs.put(entityID, new EntityList.EntityEggInfo(entityID, par3, par4));
+        EntityList.entityEggs.put(entityID, new EntityList.EntityEggInfo(entityID, par1, par2));
+        EntityRegistry.addSpawn(entityClass, par3, par4, par5, CreatureType, BiomeGenBase.beach,
+                BiomeGenBase.frozenRiver, BiomeGenBase.iceMountains,
+                BiomeGenBase.icePlains, BiomeGenBase.plains,
+                BiomeGenBase.river, BiomeGenBase.swampland, BiomeGenBase.taiga,
+                BiomeGenBase.taigaHills);
     }
 }
