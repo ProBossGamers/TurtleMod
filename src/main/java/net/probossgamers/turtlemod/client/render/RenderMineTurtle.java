042a2b49c0d63bbed970896c79f7048276cbd71d
iLexiconn <ilexiconn@gmail.com>
2014-08-11 09:48:26 +0200

    Revert "Nicer ai, fixed build file and updated gitignore"
    This reverts commit a2b441de54c0ba238be0fd3bddb3795c1459d565.

--- src/main/java/net/probossgamers/turtlemod/client/render/RenderMineTurtle.java
+++ src/main/java/net/probossgamers/turtlemod/client/render/RenderMineTurtle.java
@@ -3,22 +3,21 @@ package net.probossgamers.turtlemod.client.render;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.probossgamers.turtlemod.client.model.ModelMineTurtle;
 
 @SideOnly(Side.CLIENT)
-public class RenderMineTurtle extends RenderLiving
-{
-    public static final ResourceLocation textures = new ResourceLocation("turtlemod:textures/mobs/MineTurtle.png");
+public class RenderMineTurtle extends RenderLiving {
 
-    public RenderMineTurtle(ModelMineTurtle model, float shadowSize)
-    {
+    public static final ResourceLocation textureLocation = new ResourceLocation("turtlemod:textures/mobs/MineTurtle.png");
+
+    public RenderMineTurtle(ModelMineTurtle model, float shadowSize) {
+
         super(model, shadowSize);
     }
 
-    public ResourceLocation getEntityTexture(Entity entity)
-    {
-        return textures;
+    @Override
+    protected ResourceLocation getEntityTexture(net.minecraft.entity.Entity par1Entity) {
+        return textureLocation;
     }
 }
