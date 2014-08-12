29894fcacc92114e5585d8e88158c44cc275047e
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-12 11:17:52 +1000

    Zombie Turtle class fixed up Burn height fixed

--- src/main/java/net/probossgamers/turtlemod/entity/monster/EntityZombieTurtle.java
+++ src/main/java/net/probossgamers/turtlemod/entity/monster/EntityZombieTurtle.java
@@ -5,95 +5,57 @@ import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class EntityZombieTurtle extends EntityMob {
-
-    public EntityZombieTurtle(World par1World) {
-        super(par1World);
+    public EntityZombieTurtle(World world) {
+        super(world);
         this.getNavigator().setBreakDoors(true);
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIBreakDoor(this));
-        this.tasks.addTask(2, new EntityAIAttackOnCollide(this,
-                EntityPlayer.class, 0.5D, false));
-        this.tasks.addTask(3, new EntityAIAttackOnCollide(this,
-                EntityVillager.class, 0.5D, true));
-        this.tasks.addTask(5,
-                new EntityAIMoveThroughVillage(this, 0.5D, false));
-        this.tasks.addTask(6, new EntityAIWander(this, 0.5D));
-        this.tasks.addTask(7, new EntityAIWatchClosest(this,
-                EntityPlayer.class, 8.0F));
-        this.tasks.addTask(7, new EntityAILookIdle(this));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this,EntityPlayer.class, 0.5D, false));
+        this.tasks.addTask(3, new EntityAIAttackOnCollide(this,EntityVillager.class, 0.5D, true));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(6,new EntityAIMoveThroughVillage(this, 0.5D, false));
+        this.tasks.addTask(7, new EntityAIWander(this, 0.5D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this,EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this,
-                EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
+        this.setSize(0.6F, 0.5F);
     }
 
-    protected void applyEntityAttributes() {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.20000000298023224D);
-        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3.0D);
-    }
-
-    protected boolean isAIEnabled() {
+    public boolean isAIEnabled() {
         return true;
     }
 
-    /**
-     * Returns the sound this mob makes while it's alive.
-     */
-    protected String getLivingSound() {
+    public void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.1150000021D);
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(6.0D);
+    }
+
+    public String getLivingSound() {
         return "turtlemod:mob.turtle.living";
     }
 
-    /**
-     * Returns the sound this mob makes when it is hurt.
-     */
-    protected String getHurtSound() {
+    public String getHurtSound() {
         return "turtlemod:mob.turtle.hurt";
     }
 
-    /**
-     * Returns the sound this mob makes on death.
-     */
-    protected String getDeathSound() {
+    public String getDeathSound() {
         return "turtlemod:mob.turtle.death";
     }
 
-    /**
-     * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
-     * use this to react to sunlight and start to burn.
-     */
     public void onLivingUpdate() {
         if (this.worldObj.isDaytime() && !this.worldObj.isRemote && !this.isChild()) {
             float f = this.getBrightness(1.0F);
-
             if (f > 0.5F && this.rand.nextFloat() * 30.0F < (f - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ))) {
-                boolean flag = true;
-                ItemStack itemstack = this.getEquipmentInSlot(4);
-
-                if (itemstack != null) {
-                    if (itemstack.isItemStackDamageable()) {
-                        itemstack.setItemDamage(itemstack.getItemDamageForDisplay() + this.rand.nextInt(2));
-
-                        if (itemstack.getItemDamageForDisplay() >= itemstack.getMaxDamage()) {
-                            this.renderBrokenItemStack(itemstack);
-                            this.setCurrentItemOrArmor(4, null);
-                        }
-                    }
-
-                    flag = false;
-                }
-
-                if (flag) {
-                    this.setFire(8);
-                }
+                setFire(8);
             }
+            super.onLivingUpdate();
         }
-
-        super.onLivingUpdate();
     }
 }
