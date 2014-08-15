a44fdac5e0fa1803657acbb3907fa11b783ae1b4
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-15 16:24:13 +1000

    cant remember

--- src/main/java/net/probossgamers/turtlemod/entity/RegisterEntities.java
+++ src/main/java/net/probossgamers/turtlemod/entity/RegisterEntities.java
@@ -17,10 +17,10 @@ public class RegisterEntities {
     public static void entityInit() {
         registerEntity(EntityTurtle.class, "turtle", 0x1e8100, 0x7d3900, 10, 2, 4, EnumCreatureType.creature);
         registerEntity(EntityZombieTurtle.class, "zombieTurtle", 0x008344, 0x823F02, 10, 2, 4, EnumCreatureType.monster);
-        registerEntity(EntityMineTurtle.class, "mineTurtle", 0x1e8100, 0xef0000, 10, 2, 4, EnumCreatureType.monster);
+        registerEntity(EntityMineTurtle.class, "mineTurtle", 0x1e8100, 0xef0000, 10, 2, 4, EnumCreatureType.creature);
         registerEntity(EntityHelloGuy.class, "helloGuy", 0xffffff, 0xf90000, 10, 2, 4, EnumCreatureType.creature);
-        registerEntity(EntityDonatello.class, "donatello", 0x1e8100, 0x640087, 10, 2, 4, EnumCreatureType.monster);
-        registerEntity(EntityLeonardo.class, "leonardo", 0x1e8100, 0x002dff, 10, 2, 4, EnumCreatureType.monster);
+        registerEntity(EntityDonatello.class, "donatello", 0x1e8100, 0x640087, 10, 2, 4, EnumCreatureType.creature);
+        registerEntity(EntityLeonardo.class, "leonardo", 0x1e8100, 0x002dff, 10, 2, 4, EnumCreatureType.creature);
     }
 
     public static void registerEntity(Class<? extends EntityLiving> entityClass, String name, int par1, int par2, int par3, int par4, int par5, EnumCreatureType CreatureType) {
