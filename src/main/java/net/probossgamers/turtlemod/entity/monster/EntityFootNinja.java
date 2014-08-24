e14fe2bfc8932630da27d6573f66fb2ea883daa0
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-25 09:53:52 +1000

    Foot Ninja

--- src/main/java/net/probossgamers/turtlemod/entity/monster/EntityFootNinja.java
+++ src/main/java/net/probossgamers/turtlemod/entity/monster/EntityFootNinja.java
@@ -1,19 +1,30 @@
 package net.probossgamers.turtlemod.entity.monster;
 
-import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
-public class EntityFootNinja extends EntityCreature {
+public class EntityFootNinja extends EntityMob {
     public EntityFootNinja(World world) {
         super(world);
         tasks.addTask(0, new EntityAISwimming(this));
+        tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityDonatello.class, 1.0D, false));
+        tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityLeonardo.class, 1.0D, false));
+        tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityMichelangelo.class, 1.0D, false));
+        tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityRaphael.class, 1.0D, false));
         tasks.addTask(4, new EntityAIWander(this, 1.0D));
         tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         tasks.addTask(5, new EntityAILookIdle(this));
         targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityDonatello.class, 0, true));
+        targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityLeonardo.class, 0, true));
+        targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityMichelangelo.class, 0, true));
+        targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityRaphael.class, 0, true));
     }
 
     public boolean isAIEnabled() {
@@ -25,6 +36,7 @@ public class EntityFootNinja extends EntityCreature {
         super.applyEntityAttributes();
         getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
         getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.23000000417232513D);
+        getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(1.5D);
     }
 
 
