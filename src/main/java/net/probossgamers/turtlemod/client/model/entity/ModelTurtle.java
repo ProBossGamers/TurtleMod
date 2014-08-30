7f8f3b0e53a8b2fb627e5ee532d0232c579c7665
Ryan Eastland <ryanjeastland@gmail.com>
2014-08-31 00:32:52 +1000

    rotation point fix

--- src/main/java/net/probossgamers/turtlemod/client/model/entity/ModelTurtle.java
+++ src/main/java/net/probossgamers/turtlemod/client/model/entity/ModelTurtle.java
@@ -3,6 +3,7 @@ package net.probossgamers.turtlemod.client.model.entity;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
 
 /**
  * Ryan, you can clean up the model classes...
@@ -88,6 +89,10 @@ public class ModelTurtle extends ModelBase
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
     {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        FrontLeft.rotateAngleX = MathHelper.cos(f * 0.666F) * 1.4F * f1;
+        FrontRight.rotateAngleX = MathHelper.cos(f * 0.666F + (float) Math.PI) * 1.4F * f1;
+        BackLeft.rotateAngleX = MathHelper.cos(f * 0.666F + (float) Math.PI) * 1.4F * f1;
+        BackRight.rotateAngleX = MathHelper.cos(f * 0.666F) * 1.4F * f1;
 
         FrontRight.rotateAngleY = 0.0F;
         FrontLeft.rotateAngleY = 0.0F;
