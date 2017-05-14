f63577c020d2092dd1d857ab46965e16fd56c644
a2937 <a.rcottrill521@gmail.com>
2017-05-14 13:50:59 -0400

    Work on the AI. The entity class itself still needs to be added in to the list from the TurtleMod file.

---
+++ src/main/java/com/probossgamers/turtlemod/entities/ai/EntityAIExplodeOnCollide.java
@@ -0,0 +1,36 @@
+package com.probossgamers.turtlemod.entities.ai;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIBase;
+
+/**
+ * Created by aaron on 5/14/2017.
+ */
+public class EntityAIExplodeOnCollide extends EntityAIBase
+{
+    public Class classTarget;
+    public Entity entityClass;
+
+    public EntityAIExplodeOnCollide(EntityLiving entity, Class target)
+    {
+        classTarget = target;
+        entityClass = entity;
+    }
+
+    public boolean shouldExecute()
+    {
+        return !entityClass.world.getEntitiesWithinAABB(classTarget, entityClass.getCollisionBoundingBox()).isEmpty();
+    }
+
+    public void startExecuting()
+    {
+        explode();
+    }
+
+    public void explode()
+    {
+        entityClass.world.createExplosion(entityClass, entityClass.posX, entityClass.posY, entityClass.posZ, 10f, entityClass.world.getGameRules().getBoolean("mobGriefing"));
+        entityClass.setDead();
+    }
+}
---
+++ src/main/java/com/probossgamers/turtlemod/entities/monster/EntityMineTurtle.java
@@ -0,0 +1,68 @@
+package com.probossgamers.turtlemod.entities.monster;
+
+import com.probossgamers.turtlemod.SoundHandler;
+import com.probossgamers.turtlemod.blocks.ModBlocks;
+import com.probossgamers.turtlemod.entities.ai.EntityAIExplodeOnCollide;
+import com.probossgamers.turtlemod.items.ModItems;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+
+/**
+ * Created by aaron on 5/14/2017.
+ */
+public class EntityMineTurtle extends EntityCreature
+{
+    public EntityMineTurtle(World world)
+    {
+        super(world);
+        tasks.addTask(1, new EntityAISwimming(this));
+        tasks.addTask(2, new EntityAIExplodeOnCollide(this, EntityPlayer.class));
+        tasks.addTask(3, new EntityAIWander(this, 1.0D));
+        tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0f));
+        tasks.addTask(5, new EntityAILookIdle(this));
+        setSize(0.6f, 0.5f);
+    }
+
+    public void dropFewItems(boolean hit, int looting)
+    {
+        entityDropItem(new ItemStack(ModItems.turtleLeather), 0);
+        entityDropItem(new ItemStack(Items.GUNPOWDER), 0);
+        this.entityDropItem(new ItemStack(ModBlocks.turtleShell), 0);
+    }
+
+    public boolean isAIEnabled()
+    {
+        return true;
+    }
+
+    public void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(15.0D);
+        getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D / 2);
+    }
+
+    public SoundEvent getLivingSound()
+    {
+        return SoundHandler.turtle_living;
+    }
+
+    public SoundEvent getHurtSound()
+    {
+        return SoundHandler.turtle_hurt;
+    }
+
+    public SoundEvent getDeathSound()
+    {
+        return SoundHandler.turtle_death;
+    }
+}
