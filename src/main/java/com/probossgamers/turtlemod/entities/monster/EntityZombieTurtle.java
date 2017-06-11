ae0cead7ce1c5b9b8e2d3bf36454003fcf2b1d4e
a2937 <a.rcottrill521@gmail.com>
2017-06-11 15:15:52 -0400

    Try to add the cure to the zombie turtle. I had to go to EntityZombieVillager class in order to find it.

--- src/main/java/com/probossgamers/turtlemod/entities/monster/EntityZombieTurtle.java
+++ src/main/java/com/probossgamers/turtlemod/entities/monster/EntityZombieTurtle.java
@@ -2,27 +2,40 @@ package com.probossgamers.turtlemod.entities.monster;
 
 import com.probossgamers.turtlemod.SoundHandler;
 import com.probossgamers.turtlemod.blocks.ModBlocks;
+import com.probossgamers.turtlemod.entities.EntityTurtle;
 import com.probossgamers.turtlemod.items.ModItems;
+import net.minecraft.block.Block;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.monster.EntityZombieVillager;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.pathfinding.PathNavigateGround;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.Calendar;
@@ -34,6 +47,8 @@ import java.util.List;
 public class EntityZombieTurtle extends EntityMob
 {
     private boolean isBreakDoorsTaskSet;
+    private int conversionTime;
+    private static final DataParameter<Boolean> CONVERTING = EntityDataManager.<Boolean>createKey(EntityZombieTurtle.class, DataSerializers.BOOLEAN);
 
     private final EntityAIBreakDoor breakDoor = new EntityAIBreakDoor(this);
 
@@ -53,6 +68,12 @@ public class EntityZombieTurtle extends EntityMob
         targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, false));
         setSize(0.6F, 0.5F);
+    }
+
+    protected void entityInit()
+    {
+        super.entityInit();
+        this.dataManager.register(CONVERTING, Boolean.valueOf(false));
     }
 
     public boolean isAIEnabled()
@@ -96,8 +117,14 @@ public class EntityZombieTurtle extends EntityMob
 
 
         compound.setBoolean("CanBreakDoors", this.isBreakDoorsTaskSet());
+
+        compound.setInteger("ConversionTime", this.isConverting() ? this.conversionTime : -1);
     }
 
+    public boolean isConverting()
+    {
+        return ((Boolean)this.getDataManager().get(CONVERTING)).booleanValue();
+    }
 
     public void readEntityFromNBT(NBTTagCompound compound)
     {
@@ -106,6 +133,134 @@ public class EntityZombieTurtle extends EntityMob
 
 
         this.setBreakDoorsAItask(compound.getBoolean("CanBreakDoors"));
+        if (compound.hasKey("ConversionTime", 99) && compound.getInteger("ConversionTime") > -1)
+        {
+            this.startConverting(compound.getInteger("ConversionTime"));
+        }
+    }
+
+
+    protected void startConverting(int conversionTime)
+    {
+        this.conversionTime = conversionTime;
+        this.getDataManager().set(CONVERTING, Boolean.valueOf(true));
+        this.removePotionEffect(MobEffects.WEAKNESS);
+        this.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, conversionTime, Math.min(this.world.getDifficulty().getDifficultyId() - 1, 0)));
+        this.world.setEntityState(this, (byte)16);
+    }
+
+    protected int getConversionProgress()
+    {
+        int i = 1;
+
+        if (this.rand.nextFloat() < 0.01F)
+        {
+            int j = 0;
+            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+            for (int k = (int)this.posX - 4; k < (int)this.posX + 4 && j < 14; ++k)
+            {
+                for (int l = (int)this.posY - 4; l < (int)this.posY + 4 && j < 14; ++l)
+                {
+                    for (int i1 = (int)this.posZ - 4; i1 < (int)this.posZ + 4 && j < 14; ++i1)
+                    {
+                        Block block = this.world.getBlockState(blockpos$mutableblockpos.setPos(k, l, i1)).getBlock();
+
+                        if (block == Blocks.IRON_BARS || block == Blocks.BED)
+                        {
+                            if (this.rand.nextFloat() < 0.3F)
+                            {
+                                ++i;
+                            }
+
+                            ++j;
+                        }
+                    }
+                }
+            }
+        }
+
+        return i;
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    public void handleStatusUpdate(byte id)
+    {
+        if (id == 16)
+        {
+            if (!this.isSilent())
+            {
+                this.world.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, SoundEvents.ENTITY_ZOMBIE_VILLAGER_CURE, this.getSoundCategory(), 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F, false);
+            }
+        }
+        else
+        {
+            super.handleStatusUpdate(id);
+        }
+    }
+    /**
+     * Called to update the entity's position/logic.
+     */
+    public void onUpdate()
+    {
+        if (!this.world.isRemote && this.isConverting())
+        {
+            int i = this.getConversionProgress();
+            this.conversionTime -= i;
+
+            if (this.conversionTime <= 0)
+            {
+                this.finishConversion();
+            }
+        }
+
+        super.onUpdate();
+    }
+
+    protected void finishConversion()
+    {
+        EntityTurtle entityTurtle = new EntityTurtle(this.world);
+        entityTurtle.copyLocationAndAnglesFrom(this);
+
+
+        this.world.removeEntity(this);
+        entityTurtle.setNoAI(this.isAIDisabled());
+
+        if (this.hasCustomName())
+        {
+            entityTurtle.setCustomNameTag(this.getCustomNameTag());
+            entityTurtle.setAlwaysRenderNameTag(this.getAlwaysRenderNameTag());
+        }
+
+        this.world.spawnEntity(entityTurtle);
+        entityTurtle.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 200, 0));
+        this.world.playEvent((EntityPlayer)null, 1027, new BlockPos((int)this.posX, (int)this.posY, (int)this.posZ), 0);
+    }
+
+
+    public boolean processInteract(EntityPlayer player, EnumHand hand)
+    {
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (itemstack.getItem() == Items.GOLDEN_APPLE && itemstack.getMetadata() == 0 && this.isPotionActive(MobEffects.WEAKNESS))
+        {
+            if (!player.capabilities.isCreativeMode)
+            {
+                itemstack.shrink(1);
+            }
+
+            if (!this.world.isRemote)
+            {
+                this.startConverting(this.rand.nextInt(2401) + 3600);
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     /**
      * Called only once on an entity when first time spawned, via egg, mob spawner, natural spawning etc, but not called
